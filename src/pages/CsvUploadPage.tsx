94ae47f57e7fedac30918520618754131ceaba4e
sahara <mrjung72@gmail.com>
2025-05-20 21:59:43 +0900

    원격서버정보 csv파일 업로드 기능 추가

--- src/pages/CsvUploadPage.tsx
+++ src/pages/CsvUploadPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Layout from '../components/Layout'
+import { data } from 'react-router'
 
 const CsvUploadPage = () => {
     const [file, setFile] = useState<File | null>(null)
@@ -12,7 +13,7 @@ const CsvUploadPage = () => {
         }
     }
 
-    const handleUpload = async () => {
+const handleUpload = async () => {
         if (!file) {
             setMessage('CSV 파일을 선택하세요.')
             return
@@ -25,16 +26,23 @@ const CsvUploadPage = () => {
         setMessage('')
 
         try {
-            const response = await fetch('http://localhost:4000/api/upload-csv', {
+            const response = await fetch('http://localhost:4000/api/upload-servers', {
                 method: 'POST',
                 body: formData,
             })
 
             const data = await response.json()
-            setMessage(data.message)
+            if (data.success) {
+                setMessage(`${data.message} ${data.uploadedCount}개의 서버 정보가 업로드되었습니다.`)
+            }   
+            else {
+                setMessage('[ERROR] ' + data.error)
+            }   
+            console.log(data)
+
         } catch (err) {
-            setMessage('업로드 중 오류 발생')
             console.error(err)
+            setMessage('업로드 중 오류가 발생했습니다.')
         } finally {
             setUploading(false)
         }
@@ -42,7 +50,7 @@ const CsvUploadPage = () => {
 
     return (
         <Layout>
-            <h2>CSV 업로드</h2>
+            <h2>서버정보CSV 업로드</h2>
             <input type="file" accept=".csv" onChange={handleFileChange} />
             <button onClick={handleUpload} disabled={uploading}>
                 {uploading ? '업로드 중...' : '업로드'}
